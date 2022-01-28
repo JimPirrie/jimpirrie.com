5ce74ec4a487a852a79cfaa04ffe44ef118b7856
Jim Pirrie <jim@jimpirrie.com>
2022-01-28 16:37:32 +0000

    Initial Commit

---
+++ code/_includes/render.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+function renderPage($controller){
+
+    $data = [];
+
+    include(__DIR__."/../_controllers/{$controller}.php");
+
+    // Twig Bootstrap
+    $loader = new Twig\Loader\FilesystemLoader(__DIR__."/../_templates/");
+
+    $twig = new Twig\Environment($loader, array(
+        'cache' => __DIR__."/../cache",
+        'auto_reload' => true,
+        'debug' => true
+    ));
+    $twig->addExtension(new \Twig\Extension\DebugExtension());
+
+    // load template
+    $template = "{$controller}.html.twig";
+    $tpl = $twig->load($template);
+
+    // render template with our data
+
+    echo $tpl->render($data);
+}
---
+++ code/web/index.php
@@ -0,0 +1,25 @@
+<?php
+
+error_log(E_ERROR);
+
+require_once("init.php");
+
+$controller = str_replace("/", "", $_SERVER["REQUEST_URI"]);
+
+if($controller){
+
+    $path = "{$_SERVER["DOCUMENT_ROOT"]}/../_controllers/{$controller}.php";
+
+    if(!file_exists($path)){
+
+        $controller = "not-found";
+    }
+}
+else{
+
+    $controller = "home";
+}
+
+renderPage($controller);
+
+
