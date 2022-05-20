0951a8927e66caa7a34529c166a3fd53ab76462b
Jim Pirrie <jim@jimpirrie.com>
2022-05-20 17:24:04 +0100

    Workshop copy

---
+++ code/_controllers/workshop-follow-up-coaching.php
@@ -0,0 +1,7 @@
+workshop-foundations-of-professional-effectiveness.php<?php
+
+$twigData["seo"]["title"] = "Professional Development Coaching | Jim Pirrie Coaching";
+$twigData["seo"]["description"] = "";
+$twigData["seo"]["image"] = "";
+
+$twigData["active"]["workshops"] = "active";
