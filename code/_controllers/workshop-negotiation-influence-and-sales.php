c7ca2ceed4e7cb8025f1ab8107f564c9c0888125
Jim Pirrie <jim@jimpirrie.com>
2022-05-23 14:22:12 +0100

    Workshop copy

--- code/_controllers/workshop-negotiation-influence-and-sales.php
+++ code/_controllers/workshop-negotiation-influence-and-sales.php
@@ -10,8 +10,8 @@ $twigData["template"] = "workshop.html.twig";
 
 $twigData["active"]["negotiation"] = "_active";
 
-$twigData["title"] = "Negotiation, Mediation & Sales";
+$twigData["title"] = "Negotiation, Influence & Sales";
 $twigData["image"] = "handshake.png";
-$twigData["lead"] = "Some people's blood run cold at the word 'selling': but the skills of discovery, negotiation, mediation, persuasion and closing are at the heart of almost all professional conversations.";
+$twigData["lead"] = "Some people's blood run cold at the word 'selling': but the skills of discovery, negotiation, influence, persuasion and closing are at the heart of almost all professional conversations.";
 $twigData["summary"] = "In this workshop you'll learn the core skills that not only underpin business growth, but also allow effective business conversations at every level, from client meetings through delegation to peer collaboration and managing everyday workload demands...";
 
--- code/_controllers/workshop-strategic-decision-making.php
+++ code/_controllers/workshop-strategic-decision-making.php
@@ -8,10 +8,10 @@ $twigData["active"]["workshops"] = "active";
 
 $twigData["template"] = "workshop.html.twig";
 
-$twigData["active"]["analysis"] = "_active";
+$twigData["active"]["strategy"] = "_active";
 
-$twigData["title"] = "Strategic Analysis";
+$twigData["title"] = "Strategic Decision Making";
 $twigData["image"] = "helicopter.png";
 $twigData["lead"] = "The business world is full of contradicting viewpoints, covert agendas and innocently unspoken but vital information. As a professional you need to fill the gaps and navigate the noise to reach your own conclusions.";
-$twigData["summary"] = "In this workshop you'll experience using classic business analysis tools to analyse situations; see the bigger picture; help you filter out fact from fiction; help you to ask better questions; take a view; and make your own decisions…";
+$twigData["summary"] = "In this workshop you'll experience using classic business strategy tools to analyse situations; see the bigger picture; help you filter out fact from fiction; help you to ask better questions; take a view; and make your own decisions…";
 
