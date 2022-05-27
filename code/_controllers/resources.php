274257b8786752096f12879a433ae8e30f3366e7
Jim Pirrie <jim@jimpirrie.com>
2022-05-27 16:22:25 +0100

    Workshop copy

---
+++ code/_controllers/resources.php
@@ -0,0 +1,7 @@
+<?php
+
+$twigData["seo"]["title"] = "Professional Development Coaching | Jim Pirrie Coaching";
+$twigData["seo"]["description"] = "";
+$twigData["seo"]["image"] = "";
+
+$twigData["active"]["coaching"] = "active";
---
+++ code/_controllers/workshop-getting-things-done.php
@@ -0,0 +1,17 @@
+<?php
+
+$twigData["seo"]["title"] = "Professional Development Coaching | Jim Pirrie Coaching";
+$twigData["seo"]["description"] = "";
+$twigData["seo"]["image"] = "";
+
+$twigData["active"]["workshops"] = "active";
+
+$twigData["template"] = "workshop.html.twig";
+
+$twigData["active"]["productivity"] = "_active";
+
+$twigData["title"] = "Getting Things Done";
+$twigData["image"] = "productivity.png";
+$twigData["lead"] = "Business writing is profoundly different from what we had drummed into us at school and university: it's not about marks and grades - it's about purpose, persuasion and trust.";
+$twigData["summary"] = "In this workshop you'll learn the thinking processes and practical techniques that underpin purposeful, readable and effective business documents: from client proposals through project deliverables to website copy, the same essential principles are common to all...";
+
