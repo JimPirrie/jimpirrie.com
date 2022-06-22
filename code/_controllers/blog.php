66a599f7971b22a635ac8c2d6bb6acceeba21fbd
Jim Pirrie <jim@jimpirrie.com>
2022-06-22 14:29:33 +0100

    Blog

--- code/_controllers/blog.php
+++ code/_controllers/blog.php
@@ -37,10 +37,12 @@ if($blogPostId){
 
     */
 
+    /*
     $seoTitle = $posts[$blogPostId]["seoTitle"];
     $seoDescription = $posts[$blogPostId]["seoDescription"];
     $seoImage = $posts[$blogPostId]["seoImage"];
     $seoUrl = "{$blogPostId}/{$blogPostSlug}";
+    */
 
     $contentTemplate = "post-{$blogPostId}.html.twig";
 }
