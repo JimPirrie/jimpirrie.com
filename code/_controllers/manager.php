ffdce3baa918bd8e22d8438a374229d2cef77b73
Jim Pirrie <jim@jimpirrie.com>
2022-06-23 22:22:43 +0100

    Parse Evernote

--- code/_controllers/manager.php
+++ code/_controllers/manager.php
@@ -1,11 +1,19 @@
 <?php
 
-if($_POST["featured_sidebar"]){
+if($_POST["sort"]){
+
+    $_SESSION["sort"] = $_POST["sort"];
+
+    header("Location: /manager");
+    exit;
+}
+
+if($_POST["update_featured_sidebar"]){
 
     $esc_id = $db->real_escape_string($_POST["id"]);
     $esc_featured_sidebar = $db->real_escape_string($_POST["featured_sidebar"]);
 
-    $q = "SELECT * FROM blogPosts WHERE featured_sidebar >= $esc_featured_sidebar ORDER BY featured_sidebar";
+    $q = "SELECT * FROM blogPost WHERE featured_sidebar >= {$esc_featured_sidebar} ORDER BY featured_sidebar";
     $rs = $db->query($q);
 
     while($post = $rs->fetch_assoc()){
@@ -14,14 +22,57 @@ if($_POST["featured_sidebar"]){
         $db->query($q);
     }
 
-    $q = "UPDATE blogPost SET featured_sidebar = featured_sidebar +1 WHERE blogPost_id = {$esc_id}";
+    $q = "UPDATE blogPost SET featured_sidebar = {$esc_featured_sidebar} WHERE blogPost_id = {$esc_id}";
     $db->query($q);
+
+    $q = "SELECT * FROM blogPost WHERE featured_sidebar >0 ORDER BY featured_sidebar";
+    $rs = $db->query($q);
+
+    $i = 1;
+    while($post = $rs->fetch_assoc()){
+
+        $q = "UPDATE blogPost SET featured_sidebar = {$i} WHERE blogPost_id = {$post["blogPost_id"]}";
+        $db->query($q);
+
+        $i++;
+    }
 
     header("Location: /manager");
     exit;
 }
 
+if($_POST["update_featured_main"]){
 
+    $esc_id = $db->real_escape_string($_POST["id"]);
+    $esc_featured_main = $db->real_escape_string($_POST["featured_main"]);
+
+    $q = "SELECT * FROM blogPost WHERE featured_main >= $esc_featured_main ORDER BY featured_main";
+    $rs = $db->query($q);
+
+    while($post = $rs->fetch_assoc()){
+
+        $q = "UPDATE blogPost SET featured_main = featured_main +1 WHERE blogPost_id = {$post["blogPost_id"]}";
+        $db->query($q);
+    }
+
+    $q = "UPDATE blogPost SET featured_main = {$esc_featured_main} WHERE blogPost_id = {$esc_id}";
+    $db->query($q);
+
+    $q = "SELECT * FROM blogPost WHERE featured_main >0 ORDER BY featured_main";
+    $rs = $db->query($q);
+
+    $i = 1;
+    while($post = $rs->fetch_assoc()){
+
+        $q = "UPDATE blogPost SET featured_main = {$i} WHERE blogPost_id = {$post["blogPost_id"]}";
+        $db->query($q);
+
+        $i++;
+    }
+
+    header("Location: /manager");
+    exit;
+}
 
 if($_POST["createPost"]){
 
@@ -53,7 +104,7 @@ if($_POST["confirmDeletePost"]){
     exit;
 }
 
-if($_POST["updateStatus"]){
+if($_POST["status"]){
 
     $esc_status = $db->real_escape_string($_POST["status"]);
     $esc_id = $db->real_escape_string($_POST["id"]);
@@ -75,14 +126,55 @@ if($_POST["post"]){
     exit;
 }
 
-$q = "SELECT * FROM blogPost";
+
+if(!$_SESSION["sort"] OR $_SESSION["sort"] == "all"){
+
+    $q = "SELECT * FROM blogPost ORDER BY featured_main, title";
+}
+elseif($_SESSION["sort"] == "draft"){
+
+    $q = "SELECT * FROM blogPost WHERE status = \"draft\" ORDER BY featured_main, title";
+}
+elseif($_SESSION["sort"] == "published"){
+
+    $q = "SELECT * FROM blogPost WHERE status = \"published\" ORDER BY featured_main, title";
+}
+elseif($_SESSION["sort"] == "featured_main"){
+
+    $q = "SELECT * FROM blogPost WHERE featured_main > 0 ORDER BY featured_main";
+}
+elseif($_SESSION["sort"] == "featured_sidebar"){
+
+    $q = "SELECT * FROM blogPost WHERE featured_sidebar > 0 ORDER BY featured_sidebar";
+}
+
 $rs = $db->query($q);
 
 while($post = $rs->fetch_assoc()){
 
-    $posts[] = $post;
+    $mainList[] = $post;
 }
 
-$twigData["posts"] = $posts;
+$q = "SELECT * FROM blogPost WHERE featured_sidebar > 0 ORDER BY featured_sidebar";
+$rs = $db->query($q);
+
+while($post = $rs->fetch_assoc()){
+
+    $sidebarFeaturedList[] = $post;
+}
+
+$q = "SELECT * FROM blogPost WHERE featured_sidebar = 0 ORDER BY title";
+$rs = $db->query($q);
+
+while($post = $rs->fetch_assoc()){
+
+    $sidebarOtherList[] = $post;
+}
+
+
+$twigData["sortSelected"]["{$_SESSION["sort"]}"] = "selected";
+$twigData["mainList"] = $mainList;
+$twigData["sidebarFeaturedList"] = $sidebarFeaturedList;
+$twigData["sidebarOtherList"] = $sidebarOtherList;
 
 $contentTemplate = "manage-blog.html.twig";
--- code/_includes/_init.inc.php
+++ code/_includes/_init.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+session_start();
 error_reporting(E_ERROR);
 
 require_once(__DIR__."/../vendor/autoload.php");
