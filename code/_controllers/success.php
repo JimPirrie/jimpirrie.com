755607a37cb9e057c7fbfe33ce3594d1b1324516
Jim Pirrie <jim@jimpirrie.com>
2022-07-14 14:42:56 +0100

    Testing MVault embed

--- code/_controllers/success.php
+++ code/_controllers/success.php
@@ -4,13 +4,11 @@ $path = parse_url($_GET["actualRequest"])["path"];
 
 $parts = explode("/", $path);
 
-$twigData["mv-ref"] = $parts[2];
 
 $tomorrow = strftime("%A", strtotime("tomorrow"));
 $dayAfterTomorrow = strftime("%A", strtotime("tomorrow +1"));
 
+$twigData["mv-ref"] = $parts[2];
 $twigData["tomorrow"] = $tomorrow;
-$twigData["dayAfterTomorrow"] = $dayAfterTomorrow
-;
-$twigData["message"] = $message;
+$twigData["dayAfterTomorrow"] = $dayAfterTomorrow;
 
