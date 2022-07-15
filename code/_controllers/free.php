80eeb0af39896b7b80df3faf8b6ab185b2209683
Jim Pirrie <jim@jimpirrie.com>
2022-07-15 17:24:23 +0100

    Testing MVault embed

--- code/_controllers/free.php
+++ code/_controllers/free.php
@@ -9,6 +9,11 @@ $parts = explode("/", $path);
 $tomorrow = strftime("%A", strtotime("tomorrow"));
 $dayAfterTomorrow = strftime("%A", strtotime("tomorrow +1"));
 
+$twigData["seo"]["title"] = "Free Resources";
+$twigData["seo"]["description"] = $seoDescription;
+$twigData["seo"]["image"] = $seoImage;
+$twigData["seo"]["canonical"] = "https://www.jimpirrie.com/blog/free";
+
 $twigData["mv_ref"] = $parts[2];
 $twigData["tomorrow"] = $tomorrow;
 $twigData["dayAfterTomorrow"] = $dayAfterTomorrow;
