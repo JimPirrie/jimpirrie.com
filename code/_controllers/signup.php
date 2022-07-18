686820bb4586ff0c68e21cbbacf45434f487f74c
Jim Pirrie <jim@jimpirrie.com>
2022-07-18 16:13:25 +0100

    Testing MVault embed

---
+++ code/_controllers/signup.php
@@ -0,0 +1,14 @@
+<?php
+
+$path = parse_url($_GET["actualRequest"])["path"];
+
+$parts = explode("/", $path);
+
+
+$tomorrow = strftime("%A", strtotime("tomorrow"));
+$dayAfterTomorrow = strftime("%A", strtotime("tomorrow +1"));
+
+$twigData["mv_ref"] = $parts[2];
+$twigData["tomorrow"] = $tomorrow;
+$twigData["dayAfterTomorrow"] = $dayAfterTomorrow;
+
