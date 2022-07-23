e88e5edab8f7a2b076d1c7e6533cd9a74ea2e2b5
Jim Pirrie <jim@jimpirrie.com>
2022-07-23 17:59:42 +0100

    Testing MVault embed

--- code/_controllers/signup.php
+++ code/_controllers/signup.php
@@ -9,6 +9,7 @@ $tomorrow = strftime("%A", strtotime("tomorrow"));
 $dayAfterTomorrow = strftime("%A", strtotime("tomorrow +1"));
 
 $twigData["mv"] = json_decode(file_get_contents("https://jimpirrie.mvault.net/embed/dynamicContentServer.php?ref={$parts[2]}"), 1);
+$twigData["mv"] = json_decode(file_get_contents("https://jimpirrie.mvault.net/embed/formServer.php?formtype=2&itemRef={$parts[2]}"), 1);
 print_r($twigData["mv"]);
 
 $twigData["mv_ref"] = $parts[2];
