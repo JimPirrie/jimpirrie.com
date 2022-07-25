ed32b5183b5491b17fc3aaafc2515b40c56bbd37
Jim Pirrie <jim@jimpirrie.com>
2022-07-25 13:35:50 +0100

    Testing MVault embed

--- code/_controllers/resources.php
+++ code/_controllers/resources.php
@@ -6,6 +6,4 @@ $twigData["seo"]["image"] = "";
 
 $twigData["mv"] = json_decode(file_get_contents("https://jimpirrie.mvault.net/embed/dynamicContentServer.php?ref=all"), 1);
 
-print_r($twigData["mv"]);
-
 $twigData["active"]["resources"] = "active";
